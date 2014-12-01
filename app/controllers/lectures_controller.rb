8159546fd112d3fa0479fa92b8c110164b219e0e
ZyzioZiom <avat.delphi@gmail.com>
2014-12-01 14:04:57 +0100

    Lectures sorted by days

--- app/controllers/lectures_controller.rb
+++ app/controllers/lectures_controller.rb
@@ -1,7 +1,18 @@
 class LecturesController < ApplicationController
   def index
-  	# ordered from earliest to latest date
-  	@lectures = Lecture.order(:day)
+  	# order by earliest to latest lecture time
+  	@lectures = Lecture.order(:time)
+
+  	# find and sort lectures by days of the week
+  	@mondays = Lecture.where day: "poniedziałek"
+  	@tuesdays = Lecture.where day: "wtorek"
+  	@wednesdays = Lecture.where day: "środa"
+  	@thursdays = Lecture.where day: "czwartek"
+  	@fridays = Lecture.where day: "piątek"
+  	@saturdays = Lecture.where day: "sobota"
+  	@sundays = Lecture.where day: "niedziela"
+
+  	@sun = @sundays.count
   end
 
   def show
